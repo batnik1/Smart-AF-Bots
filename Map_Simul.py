a1a305989859fe95cacd45c94d689d88dae26c79
batnik1 <iit2019013@iiita.ac.in>
2021-08-13 12:13:10 +0530

    added colors.py and changed contrast colors

--- Map_Simul.py
+++ Map_Simul.py
@@ -1,5 +1,6 @@
 import pygame
 from Agent007 import Agent
+import colors
 pygame.init()
 # n,m=input().split()   Take input from User
 n=10   # height
@@ -14,7 +15,7 @@ screen = pygame.display.set_mode((display_height, display_width))  # create scre
 running = True
 
 def make_rect(x,y):
-    pygame.draw.rect(screen,(255, 255, 255), pygame.Rect(x, y, 5, 5))
+    pygame.draw.rect(screen,(255, 0, 0), pygame.Rect(x, y, 5, 5))
 
 
 pygame.display.set_caption("Amazon Warehouse")
@@ -48,25 +49,25 @@ def build_racks(n,m):
 def draw_line(n,m):
     x=40
     for i in range(m+1):
-        make_line(x,0,(251,255,5))
-        make_line(x+5,0,(0,255,255))
+        make_line(x,0,colors.ORANGE)                                      #up
+        make_line(x+5,0,colors.ORANGE)                                    #down
         x+=60
     y=40
     for i in range(n+1):
-        make_line(0,y,(251,255,5))
-        make_line(0,y+5,(0,255,255))
+        make_line(0,y,colors.ORANGE)                                      #right
+        make_line(0,y+5,colors.ORANGE)                                    #left
         y+=60
 
 def draw_rack_lines(x,y):
-    pygame.draw.line(screen,(251,255,5),(x+7.5,y-5),(x+7.5,y+50))
-    pygame.draw.line(screen,(251,255,5),(x+27.5,y-5),(x+27.5,y+50))
-    pygame.draw.line(screen,(0,255,255),(x+17.5,y-5),(x+17.5,y+50))
-    pygame.draw.line(screen,(0,255,255),(x+37.5,y-5),(x+37.5,y+50))
+    pygame.draw.line(screen,colors.ORANGE,(x+7.5,y-5),(x+7.5,y+50))       #up
+    pygame.draw.line(screen,colors.ORANGE,(x+27.5,y-5),(x+27.5,y+50))     #up
+    pygame.draw.line(screen,colors.ORANGE,(x+17.5,y-5),(x+17.5,y+50))     #down
+    pygame.draw.line(screen,colors.ORANGE,(x+37.5,y-5),(x+37.5,y+50))     #down
 
-    pygame.draw.line(screen,(251,255,5),(x-5,y+7.5),(x+50,y+7.5))
-    pygame.draw.line(screen,(251,255,5),(x-5,y+27.5),(x+50,y+27.5))
-    pygame.draw.line(screen,(0,255,255),(x-5,y+17.5),(x+50,y+17.5))
-    pygame.draw.line(screen,(0,255,255),(x-5,y+37.5),(x+50,y+37.5))
+    pygame.draw.line(screen,colors.ORANGE,(x-5,y+7.5),(x+50,y+7.5))              #right
+    pygame.draw.line(screen,colors.ORANGE,(x-5,y+27.5),(x+50,y+27.5))            #right
+    pygame.draw.line(screen,colors.ORANGE,(x-5,y+17.5),(x+50,y+17.5))         #left
+    pygame.draw.line(screen,colors.ORANGE,(x-5,y+37.5),(x+50,y+37.5))       #left
 
 
 a=[[20,42],[100,100],[240,300]]
@@ -82,7 +83,10 @@ while running:
     screen.blit(counter,(racks_height+200,racks_height+200))
     build_racks(n,m)
     draw_line(n,m)
-    j=(j+1)%100
+    j=(j+1)%1000
     Agent1.move([goal1[0],goal1[1]+j])
-    pygame.draw.circle(screen,(255,0,0),(Agent1.playerX,Agent1.playerY), 4)
+    pygame.draw.circle(screen,colors.LIGHTBLUE1,(Agent1.playerX,Agent1.playerY), 2)
     pygame.display.update()
+
+
+#print(colors.RED1)x
